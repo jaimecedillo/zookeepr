64615664b8f99c8d9bf5fd2c2e299ad660c306fa
jaime <jaime@MacBook-Pro.local>
2021-03-28 19:06:07 -0600

    deploy heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3001;
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -50,5 +51,5 @@ app.get('/api/animals', (req, res) => {
 
 
 app.listen(3001, () => {
-    console.log('API server now on port 3001!');
+    console.log(`API server now on port ${PORT}!`);
 });
